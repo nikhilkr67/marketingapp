17d406f51e2736b8c68a5b3195d8fd9af055d832
Nikhil KUmar <131350052+nikhilkr67@users.noreply.github.com>
2023-04-22 00:36:49 +0530

    Add files via upload

--- repository/LeadRepository.java
+++ repository/LeadRepository.java
@@ -1,1 +1,10 @@
+package com.marketingapp.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marketingapp.entities.Lead;
+
+public interface LeadRepository extends JpaRepository<Lead, Long> {
+	
+
+}
